a812c38de7c7416b494243b28dd86f84bcc34a51
HOWLADAR, ROMAN <131019051+roman0190@users.noreply.github.com>
2025-02-18 18:52:15 +0600

    edit:Metadat

--- src/components/Recommendations.tsx
+++ src/components/Recommendations.tsx
@@ -23,7 +23,55 @@ const Recommendations = () => {
           </div>
         </div>
         {/* {BUTTON} */}
-        <button className="py-1 px-4 font-semibold bg-white text-black rounded-full">Follow</button>
+        <button className="py-1 px-4 font-semibold bg-white text-black rounded-full">
+          Follow
+        </button>
+      </div>
+      {/* {USER CARD} */}
+      <div className="flex items-center justify-between">
+        {/* {AVATAR AND USER INFO} */}
+        <div className="flex items-center gap-2">
+          <div className=" relative w-10 h-10 rounded-full overflow-hidden">
+            <Image
+              path="general/avatar.png"
+              alt="avatar"
+              w={100}
+              h={100}
+              tr={true}
+            />
+          </div>
+          <div className="">
+            <h1 className="text-md font-bold">Rahat Khan</h1>
+            <span className="text-textGray text-sm">@rahat829</span>
+          </div>
+        </div>
+        {/* {BUTTON} */}
+        <button className="py-1 px-4 font-semibold bg-white text-black rounded-full">
+          Follow
+        </button>
+      </div>
+      {/* {USER CARD} */}
+      <div className="flex items-center justify-between">
+        {/* {AVATAR AND USER INFO} */}
+        <div className="flex items-center gap-2">
+          <div className=" relative w-10 h-10 rounded-full overflow-hidden">
+            <Image
+              path="general/avatar.png"
+              alt="avatar"
+              w={100}
+              h={100}
+              tr={true}
+            />
+          </div>
+          <div className="">
+            <h1 className="text-md font-bold">Rahat Khan</h1>
+            <span className="text-textGray text-sm">@rahat829</span>
+          </div>
+        </div>
+        {/* {BUTTON} */}
+        <button className="py-1 px-4 font-semibold bg-white text-black rounded-full">
+          Follow
+        </button>
       </div>
       <Link href="/" className="text-iconBlue">
         Show more
